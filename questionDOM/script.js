e0cebc0c933018b811146a6e0b1f2d92bf9a60ce
Felipe Bueno Nunes <felipebuenonunes16@gmail.com>
2021-11-19 10:20:15 -0300

    hobby

---
+++ questionDOM/script.js
@@ -0,0 +1,69 @@
+//My name in header (h1)
+const iName = document.getElementById("name");
+iName.innerText = "Felipe";
+
+//Tags li in header
+const home = document.getElementById("home");
+home.innerText = "Home";
+const sobre = document.getElementById("about");
+sobre.innerText = "Sobre"
+const hobby = document.getElementById("hobby");
+hobby.innerText = "Hobby";
+
+const picture = document.getElementById("myPicture");
+picture.src = "assets/img/picture.png";
+picture.alt = "My picture";
+
+//INFORMATION
+
+const myName = document.getElementById("myName");
+myName.innerText = "Felipe Bueno Nunes";
+
+const age = document.getElementById("age");
+age.innerText = "19 Anos";
+
+const state = document.getElementById("state");
+state.innerText = "Jacareí - São Paulo";
+
+
+//HOBBY
+const titleHobby = document.getElementById("titleHobby");
+titleHobby.innerText = "Hobbies"
+
+const descriptionHobby = document.getElementById("descriptionHobby");
+descriptionHobby.innerText = "Meu maior hobby disparadamente é o futebol, gosto tanto de asistir quanto de jogar e eu torço para o Corinthians";
+
+const video = document.getElementById("video");
+video.href = "https://www.facebook.com/watch/?v=395426654442394";
+video.innerText = "'Você não sabe o que é um palmeiras é corinthians'";
+video.target = "_blank";
+
+
+//First soccer image
+const footbalFirst = document.getElementById("footbalFirst");
+footbalFirst.src = "https://www.inglesonline.com.br/wp-content/uploads/2020/09/vocabulario-sobre-futebol-em-ingles.png";
+footbalFirst.alt = "Campo de futebol";
+
+//Second soccer image
+const footbalSecond = document.getElementById("footbalSecond");
+footbalSecond.src = "https://p2.trrsf.com/image/fget/cf/1200/628/middle/images.terra.com/2020/10/30/frm20201018006.jpg";
+footbalSecond.alt = "Neo quimica arena";
+
+//Third soccer image
+const footbalThird = document.getElementById
+("footbalThird");
+footbalThird.src = "https://www.kelvion.com/fileadmin/user_upload/Kelvion/Contentimages/References/1000x500/Refrigeration/BAI_Teaser_AllianzArena_1420x600.jpg";
+footbalThird.alt = "Allianz Arena";
+
+
+//First Link
+const soccer = document.getElementById("soccer");
+soccer.innerText = "Futebol";
+soccer.href = "https://pt.wikipedia.org/wiki/Futebol";
+soccer.target = "_blank";
+
+//Second link
+const corinthians = document.getElementById("corinthians");
+corinthians.innerText = "Corinthians";
+corinthians.href = "https://pt.wikipedia.org/wiki/Futebol";
+corinthians.target = "_blank";
